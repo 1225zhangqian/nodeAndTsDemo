88c7f1937cdf8affa8f553612cb6b90de02e4afd
QianZhang <1139384196@qq.com>
2021-06-29 11:33:39 +0800

    update the Quick Sort

--- src/view/sort.js
+++ src/view/sort.js
@@ -72,7 +72,59 @@ const selectionSort = arr => {
 console.log(selectionSort(arr));
 const arr = [1, 4, 5, 3, 2, 6, 7, 5, 7, 0, 3, 7];
 // 快速排序（Quick Sort）
-const quickSort = arr => {};
+const quickSort = (arr, left, right) => {
+  let len = arr.length,
+    partitionIndex,
+    left = typeof left != 'number' ? 0 : left,
+    right = typeof right != 'number' ? len - 1 : right;
+  if (left < right) {
+    partitionIndex = partition(arr, left, right);
+    quickSort(arr, left, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, right);
+  }
+};
+
+function partition(arr, left, right) {
+  let pivot = left,
+    index = pivot + 1;
+  for (let i = index; i <= right; i++) {
+    if (arr[i] < arr[pivot]) {
+      swap(arr, i, index);
+      index++;
+    }
+  }
+  swap(arr, pivot, index - 1);
+  return index - 1;
+}
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+function partition2(arr, low, high) {
+  let pivot = arr[low];
+  while (low < high) {
+    while (low < high && arr[high] > pivot) {
+      --high;
+    }
+    arr[low] = arr[high];
+    while (low < high && arr[low] <= pivot) {
+      ++low;
+    }
+    arr[high] = arr[low];
+  }
+  arr[low] = pivot;
+  return low;
+}
+
+function quickSort2(arr, low, high) {
+  if (low < high) {
+    let pivot = partition2(arr, low, high);
+    quickSort2(arr, low, pivot - 1);
+    quickSort2(arr, pivot + 1, high);
+  }
+  return arr;
+}
 // 归并排序（Merge Sort）
 // 堆排序（Heap Sort）
 // 计数排序（Counting Sort）
